59205333761a157b19364dda19f8d4a421835508
Maxim <Maxim@mail>
2024-06-21 03:53:39 +0300

    add test update

--- src/main/java/request/Resume.java
+++ src/main/java/request/Resume.java
@@ -9,13 +9,13 @@ import static io.restassured.RestAssured.given;
 public class Resume  extends Header {
 
 
-    private static final String ORDER_URL = BASE_URL + "negotiations/";
+    private static final String ORDER_URL = BASE_URL + "resumes/";
 
     @Step("Get order {requestBody}")
     public ValidatableResponse getOrder(String requestBody) {
         return given()
                 .spec(getRequestAuthSpec(requestBody))
-                .get(ORDER_URL).then();
+                .get(ORDER_URL+"mine").then();
     }
 
 }
